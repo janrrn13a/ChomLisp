3d49f2cceff017c0413c86f22edc69b6f44f7cb6
Janeen Reid <83963510+janrrn13a@users.noreply.github.com>
2025-04-30 07:46:11 +0000

    j

---
+++ sinterp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+
+std::string str(long n) {
+    std::ostringstream os;
+    os << n;
+    return os.str();
+}
